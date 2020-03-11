b2f5d87cc1804dd19bb14d5a29323b86d1f38910
UJJAWAL JOSHI <joshiujjawal22@gmail.com>
2020-03-11 21:25:43 +0530

    Create Army_game.java
    Luke is daydreaming in Math class. He has a sheet of graph paper with n rows and m columns, and he imagines that there is an army base in each cell for a total of  n.m bases. He wants to drop supplies at strategic points on the sheet, marking each drop point with a red dot. If a base contains at least one package inside or on top of its border fence, then it's considered to be supplied. For example:

---
+++ Army_game.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.math.*;
+import java.text.*;
+import java.util.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    /*
+     * Complete the gameWithCells function below.
+     */
+    static int gameWithCells(int n, int m) {
+        int s=0;
+        
+       if(n%2==0) s+=(n/2);
+       else s+=(n/2)+1;
+       if(m>2){
+       if(m%2==0) s=s*((m/2));
+       else s=s*((m/2)+1);
+       }
+       
+       return s;
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] nm = scanner.nextLine().split(" ");
+
+        int n = Integer.parseInt(nm[0].trim());
+
+        int m = Integer.parseInt(nm[1].trim());
+
+        int result = gameWithCells(n, m);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+    }
+}
